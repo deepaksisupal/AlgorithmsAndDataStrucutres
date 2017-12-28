cb4bcd772218540eeaea840a41e65754722170ab
Deepak <deepaksisupal@gmail.com>
2017-12-28 07:58:36 +0530

    Logging

--- AlgorithmsAndDataStructure/src/algo/sort/insertion/InsertionSort_1.java
+++ AlgorithmsAndDataStructure/src/algo/sort/insertion/InsertionSort_1.java
@@ -1,21 +1,28 @@
 package algo.sort.insertion;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 public class InsertionSort_1 {
 
+	private final static Logger LOGGER = Logger.getLogger(InsertionSort_1.class.getName());
+	
 	public static void main(String[] args) {
 		
 		int[] unsorted = { 5, 2, 4, 6, 1, 3, 2 };
 
+		LOGGER.info("Unsorted : " + Arrays.toString(unsorted));
+		
 		int[] sorted = doInsertionSort(unsorted);
 
-		System.out.println(Arrays.toString(sorted));
+		LOGGER.info("Sorted : " + Arrays.toString(sorted));
 	}
 
 	private static int[] doInsertionSort(int[] unsorted) {
 		
 		int length = unsorted.length;
+		
+		LOGGER.info("Unsorted Length: " + length);
 		
 		for (int i = 1; i < length; i++) {
 
